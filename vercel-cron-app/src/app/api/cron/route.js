b27e5bf6f52ea3f7c60e9f3b620994aca1e472e8
Mandy Jendroska <soulfreelancer@yahoo.com>
2025-04-30 15:26:43 +0200

    created vercel.json

--- vercel-cron-app/src/app/api/cron/route.js
+++ vercel-cron-app/src/app/api/cron/route.js
@@ -1,7 +1,14 @@
 import { NextResponse, NEXTResponse } from 'next/server';
 
-export async function GET() {
+export async function GET(request) {
+    const authHeader = request.headers.get('Authorization');
+    const cronSecret = process.env.CRON_SECRET;
+  
+    if (!authHeader || authHeader !== `Bearer ${cronSecret}`) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+  
     const jetzt = new Date().toISOString();
-    console.log(`Cronjob wurde ausgeführt: 'Cronjob ausgeführt um: ${jetzt}`);
-    return NextResponse.json({ nachricht: `Cronjob augeführt um: ${jetzt}` });
+    console.log(`Cronjob wurde sicher ausgeführt um: ${jetzt}`);
+    return NextResponse.json({ nachricht: `Cronjob sicher ausgeführt um: ${jetzt}` });
 }
